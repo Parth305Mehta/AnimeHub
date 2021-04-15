ee1e43c43817d06e356b125a3c6571d03c9736cd
Parth305Mehta <parthmehta305@gmail.com>
2021-04-15 19:30:54 +0530

    First commit

---
+++ imagebazaar/views.py
@@ -0,0 +1,22 @@
+from  django.http import HttpResponse
+from django.shortcuts import render
+from myapp.models import *
+from django.shortcuts import redirect
+
+
+
+def show_home_page(request):
+    cats = Category.objects.all()
+    images = Image.objects.all()
+    data = {'images':images,'cats':cats}
+    return render(request,"home.html",data)
+
+def show_category_page(request,cid):
+    cats = Category.objects.all()
+    category = Category.objects.get(pk=cid)
+    images = Image.objects.filter(cat=category)
+    data = {'images':images,'cats':cats}
+    return render(request,"home.html",data)
+
+def home(request):
+    return redirect('/home')
